92d8d2f55db07dc05cc81a462a269aa4bea7e423
Rian Ari Sasono <rianono48@gmail.com>
2022-11-10 17:23:35 +0700

    Learn Enum

---
+++ tipedata-enum.ts
@@ -0,0 +1,20 @@
+// ENUM
+enum Gender {
+  Male = 1,
+  Female = 2,
+  Unknown = -1,
+}
+
+let jenisKelamin: Gender = Gender.Female;
+
+console.log(jenisKelamin);
+
+enum Category {
+  Grocery = "Grocery",
+  Electronic = "Electronic",
+  Handphone = "Handphone",
+}
+
+let kategory: Category = Category.Electronic;
+
+console.log(kategory);
