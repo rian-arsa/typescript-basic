bfa233e5679c6b4a85060acb2bd8b80464cb814f
Rian Ari Sasono <rianono48@gmail.com>
2022-11-10 17:57:11 +0700

    Learn function as type

--- function.ts
+++ function.ts
@@ -5,6 +5,13 @@ function getName(): string {
 
 console.log(getName());
 
+// Tidak boleh mengembalikan sesuatu
+function coba(): void {
+  console.log("My Name is Rian");
+}
+
+coba();
+
 // Harus mengembalikan string dan harus ada parameter
 function hello(name: string): string {
   return `Hello ${name}`;
@@ -12,9 +19,14 @@ function hello(name: string): string {
 
 console.log(hello("Rian"));
 
-// Tidak boleh mengembalikan sesuatu
-function coba(): void {
-  console.log("My Name is Rian");
-}
+// Function sebagai type
+type Tambah = (val1: number, val2: number) => number;
 
-coba();
+const Add: Tambah = function tambahValue(
+  value1: number,
+  value2: number
+): number {
+  return value1 + value2;
+};
+
+console.log(Add(1, 2));
