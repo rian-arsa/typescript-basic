d2fae4b700df5d2ef2b790de8f09084c8b6d7e78
Rian Ari Sasono <rianono48@gmail.com>
2022-11-10 15:53:59 +0700

    Learn Tipe Data

---
+++ tipedata.ts
@@ -0,0 +1,19 @@
+// 1. Bolean
+let isMarried: boolean = true;
+
+// Error
+// isMarried = "true"
+
+// 2. Number
+let age: number = 12;
+
+// Error
+// age= true
+// age= "sepuluh"
+
+// 3. string
+let firstName: string = "Rian";
+let lastName: string = "Arsa";
+
+// Error
+// lastname = true
