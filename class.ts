bf0b216eda8d7784bc2a8ac4db6ab34724a46c5c
Rian Ari Sasono <rianono48@gmail.com>
2022-11-11 08:33:04 +0700

    Learn Static property & method

---
+++ class.ts
@@ -0,0 +1,60 @@
+// public = bisa diakses di semua class / dari luar class
+// protected = hanya bisa diakses dari class tersebut dan turunannya
+// private = hanya bisa diakses oleh class itu sendiri
+
+class User {
+  private name: string;
+
+  constructor(name: string, private age: number) {
+    this.name = name;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+}
+
+const user = new User("Rian", 20);
+// console.log(user.getName());
+
+// extends untuk menjadi children
+class Admin extends User {
+  read: boolean = true;
+  write: boolean = true;
+  private _phone: string;
+  private _email: string = "";
+
+  static getRole: string = "Admin"; // static untuk mengambil langsung tanpa initialisasi
+
+  constructor(phone: string, name: string, age: number) {
+    super(name, age); // untuk memanggil property parentnya
+    this._phone = phone;
+  }
+
+  // Setter untuk mengset property
+  public set email(v: string) {
+    this._email = v;
+  }
+
+  // Getter untuk memanggil property
+  public get email(): string {
+    return this._email;
+  }
+
+  static getRoleAdmin(): string {
+    return "Admin";
+  }
+}
+
+// const adminBaru = new Admin("1212129", "Arsa", 12);
+// console.log(adminBaru);
+
+// adminBaru.email = "rianarsa48@gmail.com";
+// console.log(adminBaru.email);
+
+const role = Admin.getRoleAdmin();
+console.log(role);
--- function.ts
+++ function.ts
@@ -3,21 +3,21 @@ function getName(): string {
   return "Rian Arsa";
 }
 
-console.log(getName());
+// console.log(getName());
 
 // Tidak boleh mengembalikan sesuatu
 function coba(): void {
   console.log("My Name is Rian");
 }
 
-coba();
+// coba();
 
 // Harus mengembalikan string dan harus ada parameter
 function hello(name: string): string {
   return `Hello ${name}`;
 }
 
-console.log(hello("Rian"));
+// console.log(hello("Rian"));
 
 // Function sebagai type
 type Tambah = (val1: number, val2: number) => number;
@@ -29,4 +29,18 @@ const Add: Tambah = function tambahValue(
   return value1 + value2;
 };
 
-console.log(Add(1, 2));
+// console.log(Add(1, 2));
+
+// Default parameter
+const getAddress = (val1: string, val2: string = "sasono"): string => {
+  return val1 + val2;
+};
+
+console.log(getAddress("Rian"));
+
+// Optional parameter
+const getMe = (val1: string, val2?: string): string => {
+  return val1 + val2;
+};
+
+console.log(getMe("Rian"));
